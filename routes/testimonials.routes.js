5c2b97037974a58b9afd5ba1011a99b82804216a
Success-akinyemi <brownsamuel915@gmail.com>
2025-03-16 21:24:43 +0100

    dele

--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -10,9 +10,6 @@ router.post('/toggleBlacklist', controllers.toggleBlacklist )
 router.post('/toggleApproveTestimony', controllers.toggleApproveTestimony )
 router.post('/deleteTestimony', AuthenticateAdmin, controllers.deleteTestimony )
 
-router.post('/dele', controllers.dele )
-
-
 //GET ROUTES
 router.get('/getAllTestimonies', controllers.getAllTestimonies )
 router.get('/getATestimonies/:id', controllers.getATestimonies )
